353cba121833852f28126b59b8a9a499c8350147
Michael Fuchs <89425629+TheMiniSandwich@users.noreply.github.com>
2024-12-20 15:18:59 -0500

    Fixing Redis issues

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ alias() {
     sudo mkdir -p /opt/nebula/bin/
     sudo mv ./nebula /opt/nebula/bin/
     sudo chmod +x /opt/nebula/bin/*
+    sudo mkdir /opt/nebula/etc/
     sudo mv ./nebula-redis.env /opt/nebula/etc/
     if ! grep -F "[FoxServers Nebula]" /etc/bash.bashrc; then
         sudo /usr/bin/bash -c 'echo "#[FoxServers Nebula]
